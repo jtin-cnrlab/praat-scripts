bd3d154a2541ba5a04a5b8355999088df4691fe1
jtin-cnrlab <42445960+jtin-cnrlab@users.noreply.github.com>
2019-06-04 14:47:01 -0400

    Use mono noise input
    Remove conversion from two-channel noise to single-channel noise (Aug 2018)

--- combine_wavs.m
+++ combine_wavs.m
@@ -1,8 +1,9 @@
 % combine_wavs.m
 % Jessica Tin
-% 27 Aug 2018
+% 28 Aug 2018
 %
-% Combines noise clips and word recordings for TFTA.
+% Combines noise clips and word recordings. Centers word within noise.
+% Created for TFTA.
 
 cd /PerrachioneLab/projects/TFTA/Stimuli/'Final transcription clips 65 dB'/
 word_files = dir('*.wav');
@@ -17,20 +18,15 @@ for i = 1:length(word_files)
     [word, Fs] = audioread(word_wav);
     [noise, Fs] = audioread(strcat(noise_dir, noise_prefix, word_wav));
 
-    %audiowrite(strcat(transcription_dir, 'stereo_', word_wav), noise, Fs);
-
-    mono_noise = sum(noise, 2) / 2;
-    %audiowrite(strcat(transcription_dir, 'mono_', word_wav), mono_noise, Fs);
-
     pad = floor((size(noise, 1) - size(word, 1))/2);
     padded_word = padarray(word, pad);
 
-    % if noise array length was odd, the padding is 1 short
-    if size(padded_word, 1) ~= size(mono_noise, 1)
+    % if word array length was odd, the padding is 1 short
+    if size(padded_word, 1) ~= size(noise, 1)
         padded_word = padarray(padded_word, 1, 'post');
     end
 
-    word_noise = mono_noise + padded_word;
+    word_noise = noise + padded_word;
 
     audiowrite(strcat(transcription_dir, transcription_prefix, word_wav), word_noise, Fs);
 end
